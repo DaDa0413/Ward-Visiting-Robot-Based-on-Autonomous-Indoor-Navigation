f389c8f07d11fda5cbe4e5b97849e08db0797762
Jadezzz <robinrobin19980202@gmail.com>
2019-07-29 14:54:48 +0800

    add new voice recognition interface

--- server_UI/app.py
+++ server_UI/app.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 
 from GUI import Ui_MainWindow
+from REC import Ui_Form
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -31,7 +32,7 @@ ADDR = ("192.168.31.64", 6666)
 
 class Voice(QThread):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, main_window=None):
         QThread.__init__(self, parent=parent)
         self.rec = False
         self.r = sr.Recognizer()
@@ -46,45 +47,46 @@ class Voice(QThread):
         print("[VOICE] Voice Thread Started")
 
         self.running = True
-
-        self.ui_parent = parent
+        self.parent = parent
+        self.ui_parent = main_window
 
     def run(self):
         while self.running == True:
             if self.rec == True:
                 with sr.Microphone() as source:
-                    print("[INFO] Say something!")
-                    audio = self.r.listen(source,phrase_time_limit=2)
+                    self.r.adjust_for_ambient_noise(source)
+                    self.parent.print_label("Say something!")
+                    try:
+                        audio = self.r.listen(source,phrase_time_limit=2, timeout=2)
+                        self.parent.print_label("Recognizing...")
+                        try:
+                            text = self.r.recognize_sphinx(audio,language="en-US", keyword_entries=self.keywords_EN)
+                            self.parent.print_label('[RES]' + text)
 
-                print("[INFO] Recognizing...")
-                try:
-                    text = self.r.recognize_sphinx(audio,language="en-US", keyword_entries=self.keywords_EN)
-                    print('[RES]' + text)
+                            if "daniel" in text:
+                                self.toggle_rec()
+                                msg = "POS " + LOCATIONS['daniel'][1] + " " + LOCATIONS['daniel'][0] 
+                                self.ui_parent.tcp_send(msg)
 
-                    if "daniel" in text:
-                        self.toggle_rec()
-                        msg = "POS " + LOCATIONS['daniel'][1] + " " + LOCATIONS['daniel'][0] 
-                        self.ui_parent.tcp_send(msg)
+                            elif "max" in text:
+                                self.toggle_rec()
+                                msg = "POS " + LOCATIONS['max'][1] + " " + LOCATIONS['max'][0]
+                                self.ui_parent.tcp_send(msg)
 
-                    elif "max" in text:
-                        self.toggle_rec()
-                        msg = "POS " + LOCATIONS['max'][1] + " " + LOCATIONS['max'][0]
-                        self.ui_parent.tcp_send(msg)
+                            elif "robin" in text:
+                                self.toggle_rec()
+                                msg = "POS " + LOCATIONS['robin'][1] + " " + LOCATIONS['robin'][0]
+                                self.ui_parent.tcp_send(msg)
 
-                    elif "robin" in text:
-                        self.toggle_rec()
-                        msg = "POS " + LOCATIONS['robin'][1] + " " + LOCATIONS['robin'][0]
-                        self.ui_parent.tcp_send(msg)
-
-                except sr.UnknownValueError:
-                    # print("Sphinx could not understand audio")
-                    pass
-                except sr.RequestError as e:
-                    # print("Sphinx error; {0}".format(e))
-                    pass
-                except sr.WaitTimeoutError as e:
-                    # print("timeout!")
-                    pass
+                        except sr.UnknownValueError:
+                            self.parent.print_label("Try Again!")
+                            time.sleep(1)
+                        except sr.RequestError:
+                            # print("Sphinx error; {0}".format(e))
+                            pass
+                    except sr.WaitTimeoutError:
+                        self.parent.print_label("Try Again!")
+                        pass
             else:
                 time.sleep(1)
     
@@ -96,20 +98,39 @@ class Voice(QThread):
 
     def stop(self):
         self.running = False
+        self.wait()
 
+class RecWindow(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super(RecWindow, self).__init__()
+        self.setupUi(self)
+        self.setGeometry(300,300, self.frameGeometry().width(), self.frameGeometry().height())
+        self.voice_th = Voice(self, main_window=parent)
+        self.print_label("Starting...")
+        self.voice_th.start()
+        self.start()
+        self.show()
+    
+    def start(self):
+        self.voice_th.toggle_rec()
+
+    def print_label(self, msg):
+        self.label.setText(msg)
+
+    def closeEvent(self, event):
+        self.voice_th.stop()
+        print("[EXIT] Voice Thread Closed")
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.setGeometry(300,300, self.frameGeometry().width(), self.frameGeometry().height())
         self.onBindingUI()
 
         self.clientsock = socket.socket()
-        self.connect()
-
-        self.voice_th = Voice(self)
-        self.voice_th.start()
+        #self.connect()
 
     def connect(self):
         try:
@@ -122,7 +143,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.btn_stop.clicked.connect(self.on_btn_stop_Clicked)
         self.btn_voice_rec.clicked.connect(self.on_btn_voice_rec_Clicked)
 
-    
 
     def tcp_send(self, msg):
         try:
@@ -153,16 +173,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
     def on_btn_voice_rec_Clicked(self):
         print("[DEBUG] VOICE REC BTN Clicked")
-        self.voice_th.toggle_rec()
+        self.rec_win = RecWindow(self)
 
         return
 
     def closeEvent(self, event):
         print("[EXIT] Close socket")
         self.clientsock.close()
-        self.voice_th.stop()
-        print("[EXIT] Closing Voice Thread")
-        time.sleep(3)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
